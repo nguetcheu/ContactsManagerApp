d1a5f0cc9dbbbfc73e0ee29cce146a7001cf36d0
nguetcheu <dnguetcheu@gmail.com>
2025-01-18 16:25:44 +0100

    Gestion des thread des operations de la base de données

--- app/src/main/java/com/example/contactsmanagerapp/Repository/RepositoryContact.java
+++ app/src/main/java/com/example/contactsmanagerapp/Repository/RepositoryContact.java
@@ -1,27 +1,54 @@
 package com.example.contactsmanagerapp.Repository;
 
+import android.os.Handler;
+import android.os.Looper;
+
 import com.example.contactsmanagerapp.Entity.ContactDAO;
 import com.example.contactsmanagerapp.Entity.Contacts;
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 public class RepositoryContact {
     // The available Data Sources:
     // -Room Database
 
     private final ContactDAO contactDAO;
+    ExecutorService executor;
+    Handler handler;
 
     public RepositoryContact(ContactDAO contactDAO) {
         this.contactDAO = contactDAO;
+
+        // Used for Background Database Operations
+         executor = Executors.newSingleThreadExecutor();
+
+        // Used for updating the UI
+        handler = new Handler(Looper.getMainLooper());
     }
 
     // Methods in DAO is being executed from Repository
     public void addContact(Contacts contact){
-        contactDAO.insert(contact);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Execute this code asynchronously
+                // on separate thread
+                contactDAO.insert(contact);
+            }
+        });
     }
 
     public void deleteContact(Contacts contact){
-        contactDAO.delete(contact);
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                contactDAO.delete(contact);
+            }
+        });
     }
 
     public List<Contacts> getAllContacts(){
