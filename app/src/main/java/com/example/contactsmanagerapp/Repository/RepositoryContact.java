51cea98a840b52aa8791b3b8bc5cdbf22a407e38
nguetcheu <dnguetcheu@gmail.com>
2025-01-18 12:18:52 +0100

    ok

--- app/src/main/java/com/example/contactsmanagerapp/Repository/RepositoryContact.java
+++ app/src/main/java/com/example/contactsmanagerapp/Repository/RepositoryContact.java
@@ -1,4 +1,30 @@
 package com.example.contactsmanagerapp.Repository;
 
-public class RespositoryContact {
+import com.example.contactsmanagerapp.Entity.ContactDAO;
+import com.example.contactsmanagerapp.Entity.Contacts;
+
+import java.util.List;
+
+public class RepositoryContact {
+    // The available Data Sources:
+    // -Room Database
+
+    private final ContactDAO contactDAO;
+
+    public RepositoryContact(ContactDAO contactDAO) {
+        this.contactDAO = contactDAO;
+    }
+
+    // Methods in DAO is being executed from Repository
+    public void addContact(Contacts contact){
+        contactDAO.insert(contact);
+    }
+
+    public void deleteContact(Contacts contact){
+        contactDAO.delete(contact);
+    }
+
+    public List<Contacts> getAllContacts(){
+        return contactDAO.getAllContacts();
+    }
 }
