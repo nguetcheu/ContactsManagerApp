b51158c33041d03b0d488036e213145b6fea9d6b
nguetcheu <dnguetcheu@gmail.com>
2025-01-18 11:55:22 +0100

    Création de la classe de base de données et liaison avec la classe du DAO, mise en place du pattern singleton de creation de base de données

--- app/src/main/java/com/example/contactsmanagerapp/ContactDatabase.java
+++ app/src/main/java/com/example/contactsmanagerapp/ContactDatabase.java
@@ -1,4 +1,13 @@
 package com.example.contactsmanagerapp;
 
-public class ContactDatabase {
+import androidx.room.Database;
+import androidx.room.RoomDatabase;
+
+@Database(entities = {Contacts.class}, version = 1)
+public abstract class ContactDatabase extends RoomDatabase {
+
+    public abstract ContactDAO getContactDAO();
+
+    // Singleton Pattern
+    
 }
