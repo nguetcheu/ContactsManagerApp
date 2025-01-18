0b3843fe90e7af8a8a066cefee82c0ac7217989c
nguetcheu <dnguetcheu@gmail.com>
2025-01-18 12:05:23 +0100

    Création de la méthode de vréation de la base de données

--- app/src/main/java/com/example/contactsmanagerapp/ContactDatabase.java
+++ app/src/main/java/com/example/contactsmanagerapp/ContactDatabase.java
@@ -1,6 +1,9 @@
 package com.example.contactsmanagerapp;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Contacts.class}, version = 1)
@@ -9,5 +12,19 @@ public abstract class ContactDatabase extends RoomDatabase {
     public abstract ContactDAO getContactDAO();
 
     // Singleton Pattern
-    
+    private static ContactDatabase dbInstance;
+
+    // method to check if any instance of database exist
+    public static synchronized ContactDatabase getInstance(Context context){
+
+        // create database if is no instance
+        if (dbInstance == null){
+            dbInstance = Room.databaseBuilder(
+                    context.getApplicationContext(),
+                    ContactDatabase.class,
+                    "contacts_db"
+            ).fallbackToDestructiveMigration().build(); // delete the database what ever is contain for each build
+        }
+        return dbInstance;
+    }
 }
